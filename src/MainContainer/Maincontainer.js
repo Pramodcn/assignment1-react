e6249c32aa91642db9ad5e84096fdb167685b7ad
pramod cn <60771694+Pramodcn@users.noreply.github.com>
2020-09-02 21:52:17 +0530

    Update Maincontainer.js

--- src/MainContainer/Maincontainer.js
+++ src/MainContainer/Maincontainer.js
@@ -17,38 +17,7 @@ export default function Maincontainer() {
 
          <div className={classes.Animation}>
              <Animation/>
-         {/* <div className={classes.AnimationFlex}>
-             <div className={classes.Arrone}>
-             <img className={classes.Tick} src={WhiteTick}/>
-                 <p className={classes.AnimationName}>Dmm Design</p>
-                 <img className={classes.UserImg} src={PinkImg} />
-                 <p>20 Dec</p>
-                 <div className={classes.ColorPink}></div>
-
-             </div>
-             <div className={classes.Arrtwo}>
-                 <img className={classes.Tick} src={BlueTick}/>
-                 <p className={classes.AnimationName}>Blurr Animation</p>
-                 <img className={classes.UserImg} src={OrangeImg} />
-                 <p>22 Dec</p>
-                 <div className={classes.ColorOrange}></div>
-             </div>
-             <div className={classes.Arrthree}>
-             <img className={classes.Tick} src={WhiteTick}/>
-                 <p className={classes.AnimationName}>Illustration</p>
-                 <img className={classes.UserImg} src={BlueImg} />
-                 <p>26 Dec</p>
-                 <div className={classes.ColorGreen}></div>
-             </div>
-             <div className={classes.Arrfour}>
-             <img className={classes.Tick} src={WhiteTick}/>
-                 <p className={classes.AnimationName}>Refunds</p>
-                 <img className={classes.UserImg} src={RedImg} />
-                 <p>28 Dec</p>
-                 <div className={classes.ColorRed}></div>
-             </div>
-         </div>  */}
-
+         
 
         </div>
 
